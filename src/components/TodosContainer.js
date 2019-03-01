e806205d3de78959b31c630c0b1c0389af27d857
dsanders_development <dsanders@them0ve.com>
2019-03-01 15:10:45 +0000

    react front-end todo

---
+++ src/components/TodosContainer.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import update from 'immutability-helper'
+
+
+    class TodosContainer extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                todos: [],
+                inputValue: ''
+            }
+    }
+    
+            createTodo = (e) => {
+              if (e.key === 'Enter') {
+                axios.post('/api/v1/tasks', {task: {title: e.target.value}})
+                .then(response => {
+                  const todos = update(this.state.todos, {
+                    $splice: [[0, 0, response.data]]
+                  })
+                  this.setState({
+                    todos: todos,
+                    inputValue: ''
+                  })
+                })
+                .catch(error => console.log(error))      
+              }   
+
+            }
+
+    getTodos() {
+        axios.get("/api/v1/tasks")
+        .then(response => { this.setState({todos: response.data}) } )
+        .catch(error => console.log(error))
+    }
+
+      handleChange = (e) => {
+         this.setState({inputValue: e.target.value});
+      }
+
+    componentDidMount() {
+        this.getTodos()
+    }
+    
+    
+    
+    render(){
+        
+        return(
+            
+            <div>
+            <div className="inputContainer">
+             <input className="" type="" placeholder="Add a Task" maxLength="50"
+                 onKeyPress={this.createTodo}  value={this.state.inputValue} onChange={this.handleChange}  />            
+            </div>
+            
+            <div className="listWrapper">
+        	   <ul className="taskList">
+        	   {this.state.todos.map((todo) => {
+        	       
+        	       return(
+        	        <li className="task" todo={todo} key={todo.id}>
+        			<input className="taskCheckbox" type="checkbox" />              
+        			<label className="taskLabel">{todo.title}</label>
+        			<span className="deleteTaskBtn">x</span>
+        		      </li>
+                	       )
+        	   } 
+        	   
+        	   )}
+        	   
+        	   
+        	   </ul>
+        	</div>
+            
+            </div>
+            
+            )
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+export default TodosContainer
